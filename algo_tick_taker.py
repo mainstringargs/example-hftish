4b99feddb5e88b2a6ce32cb57467956bfc8739d9
mainstringargs <mark.l.cloutier@gmail.com>
2019-03-15 16:47:48 -0500

    making daily clean up / init work

--- algo_tick_taker.py
+++ algo_tick_taker.py
@@ -27,7 +27,22 @@ from pylivetrader.api import (
     pipeline_output,
     schedule_function,
 )
+
+from pylivetrader import *
+
+
+from pylivetrader.misc.events import (
+    EventManager,
+    make_eventrule,
+    date_rules,
+    time_rules,
+    calendars,
+    AfterOpen,
+    BeforeClose
+)
 import os
+import threading
+import asyncio
 
 class Quote():
     """
@@ -112,29 +127,35 @@ class Position():
         self.pending_sell_shares += quantity
 
     def update_filled_amount(self, order_id, new_amount, side):
-        old_amount = self.orders_filled_amount[order_id]
-        if new_amount > old_amount:
-            if side == 'buy':
-                self.update_pending_buy_shares(old_amount - new_amount)
-                self.update_total_shares(new_amount - old_amount)
-            else:
-                self.update_pending_sell_shares(old_amount - new_amount)
-                self.update_total_shares(old_amount - new_amount)
-            self.orders_filled_amount[order_id] = new_amount
+        try:
+            old_amount = self.orders_filled_amount[order_id]
+            if new_amount > old_amount:
+                if side == 'buy':
+                    self.update_pending_buy_shares(old_amount - new_amount)
+                    self.update_total_shares(new_amount - old_amount)
+                else:
+                    self.update_pending_sell_shares(old_amount - new_amount)
+                    self.update_total_shares(old_amount - new_amount)
+                self.orders_filled_amount[order_id] = new_amount
+        except:
+            print("update_filled_amount except");                
 
     def remove_pending_order(self, order_id, side):
-        old_amount = self.orders_filled_amount[order_id]
-        if side == 'buy':
-            self.update_pending_buy_shares(old_amount - 100)
-        else:
-            self.update_pending_sell_shares(old_amount - 100)
-        del self.orders_filled_amount[order_id]
+        try:
+            old_amount = self.orders_filled_amount[order_id]
+            if side == 'buy':
+                self.update_pending_buy_shares(old_amount - 100)
+            else:
+                self.update_pending_sell_shares(old_amount - 100)
+            del self.orders_filled_amount[order_id]
+        except:
+            print("remove_pending_order except");
 
     def update_total_shares(self, quantity):
         self.total_shares += quantity
         print(str(datetime.datetime.now()) + " Total Shares of "+self.symbol +": "+str(self.total_shares));
 
-    def get_total_shares(self, quantity):
+    def get_total_shares(self):
         return self.total_shares;        
 
 conn = None;
@@ -142,7 +163,9 @@ args = None;
 api = None;
 symbol = None;
 max_shares = None;
-
+submitBuys = True;
+sessionStartPortfolioValue = None;
+stopStream = False;
 
 
 def initialize(context):
@@ -151,7 +174,7 @@ def initialize(context):
     global symbol
     global max_shares
     symbol = "TQQQ"
-    max_shares = 480
+    max_shares = 500
 
       
     opts = {}
@@ -159,10 +182,82 @@ def initialize(context):
     opts['secret_key'] = os.environ['APCA_API_SECRET_KEY']
     opts['base_url'] = os.environ['APCA_API_BASE_URL']
 
+    print(get_datetime());
+    
     global api
     api = tradeapi.REST(**opts)
-    #run()
-    schedule_functions()
+    
+    clock = api.get_clock();
+    
+
+    
+    schedule_function(
+        stop_stream,
+        date_rules.every_day(),
+        time_rules.market_close(
+            hours=0,
+            minutes=10),calendars.US_EQUITIES)
+            
+    print(str(datetime.datetime.now()) + " stop_stream Scheduled");
+
+    schedule_function(
+        cancel_open_buy_orders,
+        date_rules.every_day(),
+        time_rules.market_close(
+            hours=0,
+            minutes=15),calendars.US_EQUITIES)      
+
+    print(str(datetime.datetime.now()) + " cancel_open_buy_orders Scheduled");            
+
+    schedule_function(
+        stop_buying,
+        date_rules.every_day(),
+        time_rules.market_close(
+            hours=0,
+            minutes=20),calendars.US_EQUITIES)         
+
+
+    print(str(datetime.datetime.now()) + " stop_buying Scheduled");            
+            
+    schedule_function(
+        cancel_open_orders,
+        date_rules.every_day(),
+        time_rules.market_close(
+            hours=0,
+            minutes=7),calendars.US_EQUITIES)
+            
+    print(str(datetime.datetime.now()) + " cancel_open_orders Scheduled");
+            
+    schedule_function(
+        liquidate,
+        date_rules.every_day(),
+        time_rules.market_close(
+            hours=0,
+            minutes=5),calendars.US_EQUITIES)     
+            
+    print(str(datetime.datetime.now()) + " liquidate Scheduled");
+            
+    schedule_function(
+        end_of_day_dump,
+        date_rules.every_day(),
+        time_rules.market_close(),calendars.US_EQUITIES)          
+
+    print(str(datetime.datetime.now()) + " end_of_day_dump Scheduled");        
+
+    schedule_function(
+        start_stream,
+        date_rules.every_day(),
+        time_rules.market_open(
+            minutes=5),calendars.US_EQUITIES)
+    
+    print(str(datetime.datetime.now()) + " start_stream Scheduled");
+    
+    print("Clock "+str(clock));
+    
+    if(clock.__getattr__("is_open")):
+        print("Market is open; starting");
+        start_stream(None,None)
+    
 
 
 def run():
@@ -170,6 +265,10 @@ def run():
     global symbol
     global max_shares
     global opts 
+    global submitBuys
+    global sessionStartPortfolioValue
+    
+    submitBuys = True
     
     symbol = symbol.upper()
     quote = Quote()
@@ -188,6 +287,8 @@ def run():
         print("No current position in "+symbol);
         
     print(str(datetime.datetime.now()) + " account value at run $"+api.get_account().__getattr__("portfolio_value"));
+    
+    sessionStartPortfolioValue = api.get_account().__getattr__("portfolio_value");
 
     opts = {}
     opts['key_id'] = os.environ['APCA_API_KEY_ID']
@@ -202,11 +303,30 @@ def run():
     # Define our message handling
     @conn.on(r'Q\.' + symbol)
     async def on_quote(conn, channel, data):
+        global stopStream;
+        
+        if(stopStream):
+            #myfuture1 = asyncio.ensure_future(conn.close())
+            #loop = asyncio.get_event_loop()
+            #loop.run_until_complete(myfuture1)
+            #loop.close()
+            await conn.close();
+            return;
+    
         # Quote update received
         quote.update(data)
 
     @conn.on(r'T\.' + symbol)
     async def on_trade(conn, channel, data):
+    
+        global submitBuys
+        global stopStream;
+        
+        if(stopStream):
+            await conn.close();
+            return;
+    
+    
         if quote.traded:
             return
         # We've received a trade and might be ready to follow it
@@ -234,18 +354,19 @@ def run():
             ):
                 # Everything looks right, so we submit our buy at the ask
                 try:
-                    print(str(datetime.datetime.now()) + ' Attempting Buy @ '+str(quote.ask));
-                    o = api.submit_order(
-                        symbol=symbol, qty='100', side='buy',
-                        type='limit', time_in_force='day',
-                        limit_price=str(quote.ask)
-                    )
-                    # Approximate an IOC order by immediately cancelling
-                    api.cancel_order(o.id)
-                    position.update_pending_buy_shares(100)
-                    position.orders_filled_amount[o.id] = 0
-                    print('Buy at', quote.ask, flush=True)
-                    quote.traded = True
+                    print(str(datetime.datetime.now()) + ' Attempting Buy @ '+str(quote.ask) + ' submitBuys? '+str(submitBuys));
+                    if(submitBuys is True):
+                        o = api.submit_order(
+                            symbol=symbol, qty='100', side='buy',
+                            type='limit', time_in_force='day',
+                            limit_price=str(quote.ask)
+                        )
+                        # Approximate an IOC order by immediately cancelling
+                        api.cancel_order(o.id)
+                        position.update_pending_buy_shares(100)
+                        position.orders_filled_amount[o.id] = 0
+                        print('Buy at', quote.ask, flush=True)
+                        quote.traded = True
                 except Exception as e:
                     print(e)
             elif (
@@ -257,7 +378,7 @@ def run():
             ):
                 # Everything looks right, so we submit our sell at the bid
                 try:
-                    print(str(datetime.datetime.now()) + ' Attempting sell @ '+str(quote.bid));                
+                    print(str(datetime.datetime.now()) + ' Attempting sell @ '+str(quote.bid) + " "+str(position.total_shares)+ " "+str(position.pending_sell_shares));                
                     o = api.submit_order(
                         symbol=symbol, qty='100', side='sell',
                         type='limit', time_in_force='day',
@@ -311,9 +432,22 @@ def cancel_open_orders(context, data):
         status='open'
     )
     
+    print(open_orders)
+    
     for oo in open_orders:
-        api.cancel_order(oo.__getattr__("id"));
-        
+        print(api.cancel_order(oo.__getattr__("id")));
+
+def cancel_open_buy_orders(context, data):
+    global api
+    print(str(datetime.datetime.now()) + " Canceling all open buy orders");
+
+    open_orders = api.list_orders(
+        status='open'
+    )
+    
+    for oo in open_orders:
+        if 0 < oo.amount:  # it is a buy order
+            api.cancel_order(oo.__getattr__("id"));                
             
 def liquidate(context, data):
     global api
@@ -331,44 +465,36 @@ def liquidate(context, data):
         print(str(datetime.datetime.now()) + " Selling "+str(position));
     
 def stop_stream(context, data):
+    global stopStream;
     print(str(datetime.datetime.now()) +  " Stopping Stream");    
-    conn.close();
+    stopStream = True;
+    
+def run_thread(context):
+    event_loop = asyncio.new_event_loop();
+    asyncio.set_event_loop(event_loop)
+    run();
     
 def start_stream(context, data):
+    global stopStream;
+    stopStream = False;
     print(str(datetime.datetime.now()) +  " Starting Stream");        
-    run()    
+    t = threading.Thread(target=run_thread, args=(context,))
+    t.daemon = True
+    t.start()
+
+    
+def stop_buying(context, data):
+    global submitBuys
+    print(str(datetime.datetime.now()) +  " Stop buying");        
+    submitBuys = False    
+    
+def end_of_day_dump(context, data):
+    global sessionStartPortfolioValue
+    global api
+    print(str(datetime.datetime.now()) +  " Start of day: $" + sessionStartPortfolioValue +" End of day: $"+api.get_account().__getattr__("portfolio_value"));         
 
 def handle_data(context, data):
     pass
            
            
-def schedule_functions():
 
-    schedule_function(
-        stop_stream,
-        date_rules.every_day(),
-        time_rules.market_close(
-            hours=0,
-            minutes=10))
-
-    schedule_function(
-        cancel_open_orders,
-        date_rules.every_day(),
-        time_rules.market_close(
-            hours=0,
-            minutes=10))
-            
-    schedule_function(
-        liquidate,
-        date_rules.every_day(),
-        time_rules.market_close(
-            hours=0,
-            minutes=5))     
-
-    schedule_function(
-        start_stream,
-        date_rules.every_day(),
-        time_rules.market_open(
-            minutes=5))
-    
-    print(str(datetime.datetime.now()) + " Functions Scheduled");
